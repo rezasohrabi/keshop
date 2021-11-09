dda25afa597f594bfa83bcc3a48ce37ad1c661fb
reza sohrabi <rezasohrabi1995@yahoo.com>
2021-11-10 00:06:10 +0330

    mrege confilict

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,4 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-<<<<<<< HEAD
 
 import { NextApiRequest, NextApiResponse } from 'next'
 
@@ -8,17 +7,3 @@ const handler = (req: NextApiRequest, res: NextApiResponse) => {
 }
 
 export default handler
-=======
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
->>>>>>> 61803c11143e15bde1fc9b62961aba5ef7f28990
